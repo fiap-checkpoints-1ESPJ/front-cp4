d8891919677f0f3fdd1d23a2f1f307069f656741
Kayky Stiliano <145211344+KStiliano@users.noreply.github.com>
2024-09-03 10:03:36 -0300

    Add files via upload

---
+++ src/components/Contato.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contato = () => {
+  return (
+    <div>
+      <h1>Contato</h1>
+      <p>Entre em contato conosco pelo e-mail: lojadeesportes@contato.com</p>
+    </div>
+  );
+};
+
+export default Contato;
---
+++ src/components/Sobre.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sobre = () => {
+  const nomeLoja = " Loja de esportes"
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <img src="" alt="Sobre" />
+      <p>
+      Somos apaixonados por esportes e estamos aqui para fornecer o melhor em equipamentos e acessórios para todos os tipos de atletas. Desde os iniciantes até os profissionais, nosso objetivo é ajudar você a alcançar seus objetivos e aproveitar ao máximo o seu esporte.
+      Fundada em Ano de Fundação, nossa loja nasceu do desejo de oferecer produtos de qualidade e um atendimento excepcional. Nossa equipe é formada por especialistas que entendem suas necessidades e estão prontos para ajudar. 
+      {nomeLoja}, você encontrará tudo o que precisa, com um atendimento amigável e personalizado. Agradecemos por escolher nossa loja e esperamos fazer parte da sua jornada esportiva.
+      Vamos juntos em direção a novos desafios e conquistas!
+      </p>
+    </div>
+  );
+};
+
+export default Sobre;
